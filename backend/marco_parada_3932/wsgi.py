fefbabd80fd67c833f6f3ec263abf29482f3d243
Crowdbotics <team@crowdbotics.com>
2019-05-28 03:01:22 +0000

    initial commit

---
+++ backend/marco_parada_3932/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for marco_parada_3932 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marco_parada_3932.settings")
+
+application = get_wsgi_application()
